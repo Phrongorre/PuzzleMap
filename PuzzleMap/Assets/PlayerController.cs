fd24c34aa55d27bebe1f63bd1ef3441b843b0789
Phrongorre <34555024+Phrongorre@users.noreply.github.com>
2019-04-12 14:39:32 -0700

    Wrote PlayerController.cs mk1
    Added player to scene, made it a prefab, and wrote movement script for player. Also removed mesh from staircase simplified collider because I forgot to before. (oops)

--- PuzzleMap/Assets/PlayerController.cs
+++ PuzzleMap/Assets/PlayerController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private CharacterController CC;
+    private Camera cam;
+    private Vector3 moveVect;
+    private Vector3 turnVect;
+    private float fallVect;
+
+    public float movementSpeed = 8f;
+    public float mouseSensetivity = 4f;
+    public float gravityAcceleration = 9.8f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CC = transform.GetComponent<CharacterController>();
+        cam = transform.GetComponentInChildren<Camera>();
+        moveVect = Vector3.zero;
+        turnVect = Vector3.zero;
+        fallVect = 0f;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveVect = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        turnVect = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0f);
+
+        if (!CC.isGrounded)
+        {
+            fallVect += gravityAcceleration * Time.deltaTime;
+            moveVect += new Vector3(0f, -fallVect, 0f);
+        }
+        else fallVect = 0.1f;
+    }
+
+    void FixedUpdate()
+    {
+        CC.Move(transform.TransformVector(moveVect) * movementSpeed * Time.deltaTime);
+        CC.transform.Rotate(new Vector3(0f, turnVect.y * mouseSensetivity, 0f));
+        cam.transform.Rotate(new Vector3(turnVect.x * mouseSensetivity, 0f, 0f));
+
+        moveVect = Vector3.zero;
+        turnVect = Vector3.zero;
     }
 }
