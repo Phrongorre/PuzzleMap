8ae76b49bf3d23f4b8306f497c00dc1aeed7890a
Phrongorre <34555024+Phrongorre@users.noreply.github.com>
2019-04-16 11:44:33 -0700

    Added basic door interaction
    Opening interactions only. Bug on opening to negative angle.

---
+++ PuzzleMap/Assets/Scripts/Door.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool state;
+
+    public float closedAngle = 0f;
+    public float openAngle = 115f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = false;
+    }
+    
+    void FixedUpdate()
+    {
+        if (state)
+        {
+            transform.localEulerAngles = Vector3.Lerp(transform.localEulerAngles, new Vector3(0f, 0f, openAngle), 0.5f);
+        }
+        else
+        {
+            transform.localEulerAngles = Vector3.Lerp(transform.localEulerAngles, new Vector3(0f, 0f, closedAngle), 0.5f);
+        }
+    }
+
+    public void Open()
+    {
+        state = true;
+    }
+
+    public void Close()
+    {
+        state = false;
+    }
+}
---
+++ PuzzleMap/Assets/Scripts/InteractionController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionController : MonoBehaviour
+{
+    private Camera cam;
+
+    public float reach = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = transform.GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray aimRay = new Ray(transform.TransformPoint(cam.transform.localPosition), cam.transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(aimRay, out hit, reach))
+            {
+                if (hit.collider.CompareTag("Door"))
+                {
+                    Door door = hit.transform.GetComponent<Door>();
+                    door.Open();
+                }
+            }
+        }
+    }
+}
